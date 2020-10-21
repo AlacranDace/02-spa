98cd4cdfe0ee0e076f9e262f4be28edc245605b2
Daniel Cerezo <dnlcerezo@gmail.com>
2020-10-21 08:45:09 +0200

    Video 68. @Input - Recibir información de un componente padre a un hijo

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeTarjetaComponent implements OnInit {
 
-  constructor() { }
+  @Input() heroe: any = []; //Dice que la propiedad Heroe puede venir desde afuera
+  @Input() index: number;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  
+  verHeroe( idx: number ) {
+    //console.log(this.index);
+    this.router.navigate( ['/heroe', this.index] );
   }
 
 }
